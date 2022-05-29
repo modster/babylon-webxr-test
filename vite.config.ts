f75b7d5b59b8065709fb457c369e76d2155b8ae6
E. M. Greeff <unmodster@gmail.com>
2022-05-29 11:53:04 -0600

    added deploy.sh

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/babylon-webxr-vite/',
-    build: {
-        outDir: './docs'
-    },
 })
