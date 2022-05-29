913a49eea7403853cffe5602e1813af55a14bf5d
E. M. Greeff <unmodster@gmail.com>
2022-05-29 06:37:56 -0600

    base dir must be an absolute url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/babylon-webxr-vite/',
     build: {
         outDir: './docs'
     },
