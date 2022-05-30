854cfdf1cd6ee1460be2bf4f9d38d3ef1123aa8c
E. M. Greeff <unmodster@gmail.com>
2022-05-29 22:56:55 -0600

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:modster/babylon-webxr-test.git main:gh-pages
+git push -f git@github.com:modster/babylon-webxr-test.git master:gh-pages
 
 cd -
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,3 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: '/babylon-webxr-test/'
 })
-
-
-// import { defineConfig } from 'vite'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     port: 3443,
-//     https: true,
-//     // Uncomment to allow access from network
-//     // (or use `npm run dev -- -- host=0.0.0.0`)
-//     //host: '0.0.0.0',
-//   },
-// })
